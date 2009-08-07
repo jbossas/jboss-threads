135c777f14301603730ba5b039575d92ef4c4f58
David M. Lloyd <david.lloyd@redhat.com>
2009-08-07 19:11:46 +0000

    Allow rejecting executor to have a fixed message
    git-svn-id: https://svn.jboss.org/repos/jbossas/projects/jboss-threads/trunk/main@92151 84be2c1e-ba19-0410-b317-a758671a6fc1

--- src/main/java/org/jboss/threads/RejectingExecutor.java
+++ src/main/java/org/jboss/threads/RejectingExecutor.java
@@ -27,11 +27,18 @@ import java.util.concurrent.RejectedExecutionException;
 class RejectingExecutor implements DirectExecutor {
     static final RejectingExecutor INSTANCE = new RejectingExecutor();
 
+    private final String message;
+
     private RejectingExecutor() {
+        message = null;
+    }
+
+    RejectingExecutor(final String message) {
+        this.message = message;
     }
 
     public void execute(final Runnable command) {
-        throw new RejectedExecutionException();
+        throw new RejectedExecutionException(message);
     }
 
     public String toString() {
