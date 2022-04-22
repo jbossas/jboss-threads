9954c18749d4bef2e7e55665787e1fca89927f05
David M. Lloyd <david.lloyd@redhat.com>
2022-04-22 15:12:23 -0500

    Add `module-info`

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module org.jboss.threads {
+    requires java.management;
+    requires jdk.unsupported;
+    requires org.jboss.logging;
+    requires static org.jboss.logging.annotations;
+    requires org.wildfly.common;
+    requires io.smallrye.common.annotation;
+    requires io.smallrye.common.constraint;
+    requires io.smallrye.common.cpu;
+    requires io.smallrye.common.function;
+
+    exports org.jboss.threads;
+    exports org.jboss.threads.management;
+}
