8c87402c7a195c29e6debcad6a8668724cb08cc7
David M. Lloyd <david.lloyd@redhat.com>
2024-02-13 09:13:54 -0600

    Require JDK 11 or later

--- src/main/java/org/jboss/threads/JDKSpecific.java
+++ src/main/java/org/jboss/threads/JDKSpecific.java
@@ -1,29 +1,17 @@
 package org.jboss.threads;
 
-import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalUnit;
 import java.util.concurrent.TimeUnit;
-
-import org.wildfly.common.Assert;
 
 /**
  */
 final class JDKSpecific {
 
     static TemporalUnit timeToTemporal(final TimeUnit timeUnit) {
-        switch (timeUnit) {
-            case NANOSECONDS: return ChronoUnit.NANOS;
-            case MICROSECONDS: return ChronoUnit.MICROS;
-            case MILLISECONDS: return ChronoUnit.MILLIS;
-            case SECONDS: return ChronoUnit.SECONDS;
-            case MINUTES: return ChronoUnit.MINUTES;
-            case HOURS: return ChronoUnit.HOURS;
-            case DAYS: return ChronoUnit.DAYS;
-            default: throw Assert.impossibleSwitchCase(timeUnit);
-        }
+        return timeUnit.toChronoUnit();
     }
 
     static void onSpinWait() {
-        // no operation
+        Thread.onSpinWait();
     }
 }
